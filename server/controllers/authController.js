eb3bc518d285f3c997cab2ca212d23a91ec0a716
Vikas <vikasrjc7@gmail.com>
2026-08-03 23:35:01 +0530

    Added Authorization and login

---
+++ server/controllers/authController.js
@@ -0,0 +1,75 @@
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+
+//Register User
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                message: "Please fill all fields",
+            });
+        }
+
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({
+                message: "User already exists",
+            });
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+        });
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+// Login User
+export const loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (user && (await user.matchPassword(password))) {
+            return res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id),
+            });
+        }
+
+        res.status(401).json({
+            message: "Invalid email or password",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+//Current User
+export const getMe = async (req, res) => {
+    res.json(req.user);
+};
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -20,11 +21,12 @@ app.get("/", (req, res) => {
     res.send("FoodSnap API is Running...");
 });
 
-
+// Routes
+app.use("/api/auth", authRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(` Server running on http://localhost:${PORT}`);
 });
