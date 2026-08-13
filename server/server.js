08d4e240e6dc6a61b9c792076af8ac61c9fbdcd7
Vikas <vikasrjc7@gmail.com>
2026-08-13 23:13:33 +0530

    Add request log

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+    console.log(`New Request: ${req.method}`);
+    next();
+});
 // Home Route
 app.get("/", (req, res) => {
     res.send("FoodSnap API is Running...");
