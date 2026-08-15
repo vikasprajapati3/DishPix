9670c5dba757bd9acf0a45ec1b9be32ab4bff251
Vikas <vikasrjc7@gmail.com>
2026-08-15 15:27:41 +0530

    integrating frontend with backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
 
         try {
             const response = await axios.post(
-                "http://localhost:5000/api/auth/login",
+                `${import.meta.env.VITE_API_URL}/api/auth/login`,
                 {
                     email,
                     password,
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -23,7 +23,7 @@ export default function Register() {
             setLoading(true);
 
             const response = await axios.post(
-                "http://localhost:5000/api/auth/register",
+                `${import.meta.env.VITE_API_URL}/api/auth/register`,
                 {
                     username,
                     email,
