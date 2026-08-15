793968ae96f6cb5e106469d795d2a986285e2907
Vikas <vikasrjc7@gmail.com>
2026-08-15 20:38:40 +0530

    fix login page error

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 import axios from "axios";
 import foodBg from "../assets/food_bg1.png";
 import { FaChevronLeft } from "react-icons/fa";
