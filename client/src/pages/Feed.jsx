15ca304bcdd477cafee1ad4220b1685eed6c602b
Vikas <vikasrjc7@gmail.com>
2026-08-17 20:59:59 +0530

    added Feed page UI

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
 
 export default function Feed() {
+
     return (
-        <h2 className="text-center text-3xl font-bold">Feed Page</h2>
+        <div className="p-4">
+
+            <h1 className="text-2xl font-bold">
+                Dish<span className="text-red-500">Pix</span>
+            </h1>
+
+            <div className="bg-white p-4 mt-4">
+
+                <h2>victor</h2>
+
+                <p>Pizza Haven - Downtown</p>
+
+                <img
+                    src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGl6emF8ZW58MHx8MHx8fDA%3D"
+                    alt="Food"
+                    className="w-full h-64 object-cover"
+                />
+
+                <div>
+                    <button>❤️ 142</button>
+                    <button> 💬 18</button>
+                </div>
+
+                <p>
+                    Best Pizza I have had all year!
+                </p>
+
+            </div>
+
+        </div>
     );
 }
