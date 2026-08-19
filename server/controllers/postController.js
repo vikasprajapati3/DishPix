2f6a9aa7b2bec80ff838e37595634861edebb932
Vikas Prajapati <vikasrjc7@gmail.com>
2026-08-19 23:11:43 +0530

    Implement createPost function in postController

---
+++ server/controllers/postController.js
@@ -0,0 +1,30 @@
+
+import Post from "../models/Post.js";
+
+// new post
+export const createPost = async (req, res) => {
+    try {
+        const { restaurant, foodName, caption, rating, image } = req.body;
+
+        const post = new Post({
+            userId: req.user._id,
+            restaurant: restaurant,
+            foodName: foodName,
+            caption: caption,
+            rating: rating,
+            image: image,
+        });
+
+        await post.save();
+
+        res.status(201).json({
+            message: "Post created successfully",
+            post: post,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
