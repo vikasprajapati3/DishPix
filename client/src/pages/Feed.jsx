4e88a0446d93bd467f67d47c07ab65737025ca53
Vikas <vikasrjc7@gmail.com>
2026-08-27 23:35:00 +0530

    update feed UI

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -18,7 +18,7 @@ export default function Feed() {
                 <img
                     src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGl6emF8ZW58MHx8MHx8fDA%3D"
                     alt="Food"
-                    className="w-full h-64 object-cover"
+                    className="w-full h-auto object-cover"
                 />
 
                 <div>
