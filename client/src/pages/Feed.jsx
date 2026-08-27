d5c64e9684fce5b6423ec87eaeca0b1d19becbe6
Vikas <vikasrjc7@gmail.com>
2026-08-28 00:06:18 +0530

    add create post button in feed

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -24,12 +24,20 @@ export default function Feed() {
     ];
 
     return (
+
         <div className="p-4 bg-gray-100 min-h-screen">
 
-            <h1 className="text-2xl font-bold">
-                Dish<span className="text-red-500">Pix</span>
-            </h1>
-
+            <div className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl font-bold">
+                    Dish<span className="text-red-500">Pix</span>
+                </h1>
+                <button
+                    // onClick={ }
+                    className="bg-red-500 text-white px-4 py-2 rounded-lg"
+                >
+                    + Create Post
+                </button>
+            </div>
             {posts.map((post) => (
                 <div
                     key={post.id}
