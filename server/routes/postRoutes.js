d9113aaccfac42adfa9140f00cb9faf8bf11af5d
Vikas <vikasrjc7@gmail.com>
2026-08-29 22:55:13 +0530

    add routes for create post

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createPost } from "../controllers/postController.js";
+import protect from "../middleware/authMiddleware.js";
+import upload from "../middleware/uploadMiddleware.js";
+
+const router = express.Router();
+
+
+router.post("/", protect, upload.single("image"), createPost);
+
+export default router;
