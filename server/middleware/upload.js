c3a1a2e12ede0c14647fc3a26de86ff83dbbfed0
Vikas <vikasrjc7@gmail.com>
2026-08-29 22:03:03 +0530

    update upload middleware

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,7 +1,16 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const upload = multer({
-    dest: "uploads/"
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+
+    params: {
+        folder: "DishPix",
+        allowedFormats: ["jpg", "jpeg", "png"],
+    },
 });
+
+const upload = multer({ storage: storage });
 
 export default upload;
