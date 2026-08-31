cd43b5252452900b03d3a17f5ac86cb9ea3b4301
Vikas <vikasrjc7@gmail.com>
2026-08-31 23:27:15 +0530

    fix imports

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
+import "dotenv/config";
+
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 
-dotenv.config();
+
 
 const app = express();
 
@@ -13,14 +14,8 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://dishpix.vercel.app"
-    ],
-    methods: ["GET", "POST"],
-    credentials: true
-}));
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
@@ -37,6 +32,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
     console.log(` Server running on http://localhost:${PORT}`);
