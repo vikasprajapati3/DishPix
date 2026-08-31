7fbf47fcd60826e9011167ec6893df1a6e34bd09
Vikas <vikasrjc7@gmail.com>
2026-08-31 21:35:40 +0530

    add diskStorage in multer

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,16 +1,30 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
+import fs from "fs";
+import path from "path";
 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
 
-    params: {
-        folder: "Dishpix/Posts",
-        allowedFormats: ["jpg", "jpeg", "png"],
-    },
+const storage = multer.diskStorage({
+    destination: "uploads/",
+
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const name = path.basename(file.originalname, ext);
+
+        let filename = file.originalname;
+        let count = 1;
+
+        while (fs.existsSync(path.join("uploads", filename))) {
+            filename = `${name}(${count})${ext}`;
+            count++;
+        }
+
+        cb(null, filename);
+    }
 });
 
-const upload = multer({ storage: storage });
+
+const upload = multer({
+    storage,
+});
 
 export default upload;
