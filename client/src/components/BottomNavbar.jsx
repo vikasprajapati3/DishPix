9bc1dc40316c997329bd3f687dc9f96da6336125
Vikas <vikasrjc7@gmail.com>
2026-09-03 00:35:46 +0530

    fix UI

--- client/src/components/BottomNavbar.jsx
+++ client/src/components/BottomNavbar.jsx
@@ -12,7 +12,7 @@ const BottomNavbar = () => {
     const navigate = useNavigate();
 
     return (
-        <nav className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 px-6 py-16 md:hidden">
+        <nav className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 px-6 py-3 md:hidden">
             <div className="flex items-center justify-between">
 
                 <button
