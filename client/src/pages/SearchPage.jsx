94914a974ca82aba1f46e07b30bcb72b60dea616
Vikas <vikasrjc7@gmail.com>
2026-09-08 23:17:34 +0530

    add search page

---
+++ client/src/pages/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import BottomNavbar from "../components/BottomNavbar";
+
+export default function Search() {
+    return (
+        <>
+            <h3>This page is in maintainance</h3>
+            <BottomNavbar />
+        </>
+    )
+}
