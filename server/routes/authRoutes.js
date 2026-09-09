7e2db0202ff3d369e948224924d75058228e64bd
Vikas <vikasrjc7@gmail.com>
2026-09-10 00:19:16 +0530

    add update profile route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,12 @@
 import express from "express";
-
-import { registerUser, loginUser, getMe, } from "../controllers/authController.js";
-
+import { registerUser, loginUser, getMe, updateProfile } from "../controllers/authController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-
 router.post("/login", loginUser);
-
 router.get("/me", protect, getMe);
+router.put("/update", protect, updateProfile);
 
 export default router;
